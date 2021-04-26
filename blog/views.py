74cbaac6719d7c644cda3688d6f0a6272ba1e1d1
realdox <realdox2yk47jay@gmail.com>
2021-04-26 20:32:42 +0100

    complete text

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+# Create your views here.
 from .models import Post
 from django.views.generic import (
     ListView,
--- realdox/settings.py
+++ realdox/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xg_45kmu$jt=c&i*xl%i@xyf-=a1@r(x95ne0+-=4+k2v)89fsg$gi'
+SECRET_KEY = 'django-insecure-xg_4524252k$jt=c&i*xl%i@xyf-=a1@r(x95ne0+-=4+k2v)7789fsg$gi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
